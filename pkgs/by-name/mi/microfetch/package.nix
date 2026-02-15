180b69c30ab127cf56872fd046cb2725916cedef
nydragon <git@ccnlc.eu>
2026-02-15 16:37:21 +0100

    WIP

--- pkgs/by-name/mi/microfetch/package.nix
+++ pkgs/by-name/mi/microfetch/package.nix
@@ -1,38 +1,49 @@
 {
   lib,
-  rustPlatform,
-  mold,
+  llvm,
   stdenv,
+  stdenvAdapters,
+  rustPlatform,
   fetchFromGitHub,
   nix-update-script,
-}:
+  useMold ? stdenv.isLinux,
+}: let
+  stdenv =
+    if useMold
+    then stdenvAdapters.useMoldLinker llvm.stdenv
+    else llvm.stdenv;
+in
+  rustPlatform.buildRustPackage.override {inherit stdenv;} (self: {
+    pname = "microfetch";
+    version = "0.4.13";
 
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "microfetch";
-  version = "0.4.13";
+    src = fetchFromGitHub {
+      owner = "NotAShelf";
+      repo = "microfetch";
+      tag = self.version;
+      hash = "sha256-aJ2QuMbUM/beMD8b62AqzTNljQ8RtBNOSvj9nJfRXbA=";
+    };
 
-  src = fetchFromGitHub {
-    owner = "NotAShelf";
-    repo = "microfetch";
-    tag = "${finalAttrs.version}";
-    hash = "sha256-aJ2QuMbUM/beMD8b62AqzTNljQ8RtBNOSvj9nJfRXbA=";
-  };
+    cargoHash = "sha256-vGvpjRJr4ez322JWUwboVml22vnRVRlwpZ9W4F5wATA=";
+    enableParallelBuilding = true;
+    buildNoDefaultFeatures = true;
+    passthru.updateScript = nix-update-script {};
 
-  cargoHash = "sha256-vGvpjRJr4ez322JWUwboVml22vnRVRlwpZ9W4F5wATA=";
+    #Only set RUSTFLAGS for mold if useMold is enabled
+    env = lib.optionalAttrs useMold {
+      CARGO_LINKER = "clang";
+      RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+    };
 
-  nativeBuildInputs = lib.optionals stdenv.isLinux [ mold ];
-
-  passthru.updateScript = nix-update-script { };
-
-  meta = {
-    description = "Microscopic fetch script in Rust, for NixOS systems";
-    homepage = "https://github.com/NotAShelf/microfetch";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [
-      nydragon
-      NotAShelf
-    ];
-    mainProgram = "microfetch";
-    platforms = lib.platforms.linux;
-  };
-})
+    meta = {
+      description = "Microscopic fetch script in Rust, for NixOS systems";
+      homepage = "https://github.com/NotAShelf/microfetch";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [
+        nydragon
+        NotAShelf
+      ];
+      mainProgram = "microfetch";
+      platforms = lib.platforms.linux;
+    };
+  })
